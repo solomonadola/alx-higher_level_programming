43a79bf09ea0bc61185c264efbc038dfe8086351
solomonadola <solomonadola84@gmail.com>
2023-10-05 14:47:47 -0700

    magic

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -7,7 +7,7 @@ def magic_calculation(a, b):
     c = 0
     if a < b:
         c = add(a, b)
-        for i in range(4, 7):
+        for i in range(4, 6):
             c = add(c, i)
     else:
         c = sub(a, b)
