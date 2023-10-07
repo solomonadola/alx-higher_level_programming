874cb0adf44d35265a2e133d23b7fef2fa55e603
solomonadola <solomonadola84@gmail.com>
2023-10-07 07:41:28 -0700

    max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    mx = my_list[0]
+    for el in my_list:
+        if el > mx:
+            mx = el
+    return mx
