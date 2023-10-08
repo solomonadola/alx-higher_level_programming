894a1d0b2acd682b829ce9eafec70281c0d787f8
solomonadola <solomonadola84@gmail.com>
2023-10-08 11:09:59 -0700

    Python list info

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,14 @@
+#include <Python.h>
+
+void print_python_list_info(PyObject *p) {
+    Py_ssize_t size = PyList_Size(p);
+    Py_ssize_t allocated = ((PyListObject *)p)->allocated;
+    printf("[*] Size of the Python List = %ld\n", size);
+    printf("[*] Allocated = %ld\n", allocated);
+
+    for (Py_ssize_t i = 0; i < size; i++) {
+        PyObject *item = PyList_GetItem(p, i);
+        const char *typeName = Py_TYPE(item)->tp_name;
+        printf("Element %ld: %s\n", i, typeName);
+    }
+}
