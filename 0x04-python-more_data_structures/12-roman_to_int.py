95ab46dc2125dde4c14221aecc0fcc4a6e614c3a
solomonadola <solomonadola84@gmail.com>
2023-10-11 09:56:46 -0700

    Roman

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    if type(roman_string) is not str or roman_string is None:
+        return 0
+
+    roman = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    int_val = 0
+    for i in range(len(roman_string)):
+        if i > 0 and roman[roman_string[i]] > roman[roman_string[i - 1]]:
+            int_val += roman[roman_string[i]] - 2 * roman[roman_string[i - 1]]
+        else:
+            int_val += roman[roman_string[i]]
+    return int_val
