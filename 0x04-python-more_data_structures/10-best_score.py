9580451894c94081953c0654b62d1c701f33cbc0
solomonadola <solomonadola84@gmail.com>
2023-10-11 08:22:11 -0700

    best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    if a_dictionary is None or len(a_dictionary) == 0:
+        return None
+    max = 0
+    best = ''
+    for (key, value) in a_dictionary.items():
+        if value > max:
+            max = value
+            best = key
+    return best      
