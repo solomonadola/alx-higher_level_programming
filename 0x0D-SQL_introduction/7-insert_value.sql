412b18fa7e9a5a3570b7d307609302fb1f971c9d
solomonadola <solomonadola84@gmail.com>
2023-12-14 23:17:35 +0300

    Task 7

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
--- Insert to a table
-INSERT INTO `first_table` (`id`, `name`) VALUES ('89', "BEST SCHOOL");
+-- a script that inserts a new row to the table
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
