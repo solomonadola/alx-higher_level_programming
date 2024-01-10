41a9b96d5901bf97537f5e6a30214090ca06f192
solomonadola <solomonadola84@gmail.com>
2024-01-10 01:20:22 -0800

    Second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,13 +6,13 @@ if (argv.length === 2) {
 } else if (argv.length === 3) {
   console.log(0);
 } else {
-  let firstMax = 1;
-  let secondMax = 0;
+  let firstMax = -1000000000000000000000000000;
+  let secondMax = -100000000000000000000000000;
   for (let i = 2; i < argv.length; i++) {
     if (Number(argv[i]) > firstMax) {
       firstMax = Number(argv[i]);
     } else if (Number(argv[i]) > secondMax) {
-        secondMax = Number(argv[i]);
+      secondMax = Number(argv[i]);
     }
   }
   console.log(secondMax);
