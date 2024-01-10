3ce3d2aaebd97368c04f69d9745c43cff2cd1b8b
solomonadola <solomonadola84@gmail.com>
2024-01-10 00:44:07 -0800

    Task 6 still annoying me

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (const lang of languages) {
   console.log(lang);
 }
