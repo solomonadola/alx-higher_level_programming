5b13a9583588af0ba3314f46459d513c0a74c342
solomonadola <solomonadola84@gmail.com>
2024-01-10 10:58:32 +0300

    Formatted

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 const { argv } = require('process');
 if (argv.length <= 2) {
   console.log('No argument');
-} else if (argv.length == 3) {
+} else if (argv.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
