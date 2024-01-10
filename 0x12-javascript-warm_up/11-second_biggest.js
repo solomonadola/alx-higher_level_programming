87c78bdf099e46b007494f5bd0edc3b4f4610f15
solomonadola <solomonadola84@gmail.com>
2024-01-10 01:56:36 -0800

    Second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const { argv } = require('process');
-if (argv.length <= 3) {
-  console.log(1);
+const args = process.argv.slice(2).map(Number);
+
+if (args.length <= 1) {
+  console.log(0);
 } else {
-  const numbers = argv.slice(2, argv.length)
-    .map((num) => Number(num)).sort((x, y) => x - y);
-  console.log(numbers[numbers.length - 2]);
+  const sortedArgs = args.sort((a, b) => b - a);
+  console.log(sortedArgs[1]);
 }
