43de6c913618608add3abbbcbdd19eac1599a098
solomonadola <solomonadola84@gmail.com>
2024-01-10 00:37:36 -0800

    Task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -3,6 +3,6 @@ const languages = [
   'C is fun', 'Python is cool',
   'Javascript is amazing'
 ];
-for (let i = 0; i < languages.length; i++) {
-  console.log(languages[i]);
+for (const lang in languages) {
+  console.log(languages[lang]);
 }
