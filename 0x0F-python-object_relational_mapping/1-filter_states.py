70cd326f60a306cda9b43af8cc3dd59824583121
solomonadola <solomonadola84@gmail.com>
2024-01-17 05:37:11 -0800

    Filter

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+This module lists all states from a database whose names start with 'N'.
+"""
+
+import MySQLdb
+import sys
+
+
+def list_states(username, password, database):
+    """
+    This function connects to a MySQL server and lists
+    all states from a database whose names start with 'N'.
+    """
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=username, passwd=password, db=database)
+    cursor = db.cursor()
+    query = "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC"
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    """
+    Main function that takes command line arguments
+    and calls the function list_states.
+    """
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    list_states(username, password, database)
