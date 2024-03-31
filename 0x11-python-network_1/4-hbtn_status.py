4125ebf0c0a64dcc615e9d0c5e50d661b3fcdd04
solomonadola <solomonadola84@gmail.com>
2024-03-31 07:26:56 -0700

    Fix task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,6 +4,7 @@ a Python script that fetches https://alx-intranet.hbtn.io/status
 """
 import requests
 
+
 if __name__ == '__main__':
     url = "https://intranet.hbtn.io/status"
     r = requests.get(url)
