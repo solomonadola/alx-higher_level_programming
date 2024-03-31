6e6ec50d7f3ce6121d45f10748935ef324c176fd
solomonadola <solomonadola84@gmail.com>
2024-03-31 07:45:05 -0700

    Task 10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -9,5 +9,5 @@ from sys import argv
 
 if __name__ == '__main__':
     url = 'https://api.github.com/users/{}'.format(argv[1])
-    r = requests.get(url,auth=HTTPBasicAuth(argv[1], argv[2]))
+    r = requests.get(url, auth=HTTPBasicAuth(argv[1], argv[2]))
     print(r.json().get('id'))
